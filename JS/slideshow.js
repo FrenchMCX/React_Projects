3806c8135114cdcdcdf7522369dea0626d33313c
Tyler French <81813288+FrenchMCX@users.noreply.github.com>
2022-05-05 12:47:14 -0400

    .

---
+++ JS/slideshow.js
@@ -0,0 +1,14 @@
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function () {
+    $('#slideshow > div:first')
+        .fadeOut(1000)
+        .next()
+        .fadeIn(1000)
+        .end()
+        .appendTo('#slideshow');
+}, 3000);
